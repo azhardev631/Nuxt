97a5f9cf352a3673a001d84fc64c9f61306e939f
azhardev <azharmehmood74600@gmail.com>
2024-08-08 15:10:05 +0500

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur repellendus saepe provident minima culpa! Alias voluptatum a officiis eligendi perspiciatis.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur repellendus saepe provident minima culpa! Alias voluptatum a officiis eligendi perspiciatis.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+    p {
+        margin: 20px 0;
+    }
+</style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <ProductDetails :product="product"/>
+    </div>
+</template>
+
+<script setup>
+    const { id } = useRoute().params;
+    definePageMeta({
+        layout:'products'
+    })
+
+    const uri =  'https://fakestoreapi.com/products/' + id;
+    const {data: product} =  await useFetch(uri)
+
+    if(!product.value){
+        throw createError({statusCode:404, statusMessage: 'Product Not Found'});
+    }
+</script>
+
+<style scoped>
+
+</style>
